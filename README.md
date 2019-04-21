6198268680e6bf4a95fea0cf5e416a457175d0d2
James Pickering <james_pic@hotmail.com>
2019-04-21 17:44:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Check Companies
+===============
+
+Check which companies contributed to a project, by analysing the email
+addresses used in commits.
+
+Checkout the repo you want to check, then scan with
+
+```
+./check_companies.py /path/to/repo
+```
---
+++ check_companies.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import argparse
+import git
+import pudb
+import progressbar
+
+from collections import Counter
+
+def analyse_repo(location):
+    repo = git.Repo(location)
+    pbar = progressbar.ProgressBar()
+    return analyse_commits(pbar(list(repo.iter_commits())))
+
+def analyse_commits(commits):
+    counter = Counter()
+    for commit in commits:
+        email = commit.author.email
+        if email:
+            domain = email.rsplit('@', 1)[-1]
+        else:
+            domain = ''
+        counter[domain] += commit.stats.total['lines']
+    return dict(sorted(counter.items(), key=lambda x: -x[1]))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Identify which companies contribute to a Git project"
+    )
+    parser.add_argument(
+        'repo_location', default='.', help="Location of repo to analyse"
+    )
+    args = parser.parse_args()
+    for org, count in analyse_repo(args.repo_location).items():
+        print(org, count)
